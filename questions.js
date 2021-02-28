614fdbaaa92018483f93e3c40f1e736a10b9ce7b
yasmeenkhalili <jasmina201132@yahoo.com>
2021-02-28 16:26:08 +0200

    make web page with user inputs

---
+++ questions.js
@@ -0,0 +1,18 @@
+let Name=prompt(" What is your name ? ");
+alert ("Welcome"+ " "+ Name);
+console.log(Name);
+let Rate=prompt(" How would you rate 2020 from 10 ? ");
+parseInt(Rate);
+if (Rate>7 && Rate<=10){
+    alert ("Cool you had a nice year, congrates! ");
+    console.log(Rate);
+}else{
+    alert ("It is look like was a bad year for you! Sorry for that.");
+    console.log(Rate);
+}
+let Wishes=prompt(" what do you whish happen in 2021 ? ");
+alert ("That is a nice wishes hope to you make them true");
+console.log(Wishes);
+let Plan=prompt(" How do you make these wishes come true ? ");
+alert("May Allah make them true and give you the energy to do it");
+console.log(Plan);
